c37d27806e1ba53e1946b0375f8d45968e09617b
u1005 <u1005@ijuzhong.com>
2018-09-26 14:57:17 +0800

    test pre-commit

--- index.php
+++ index.php
@@ -16,21 +16,44 @@
 namespace pangzi
 
 // use admin\pangzi
+/**
+ * Class comment
+ * 
+ * PHP version 7.1
+ * 
+ * @category AaldkFakdkd
+ * @package  AdkFkd
+ * @author   Xia Gw <xiagw@qq.com>
+ * @license  ladk http://license.qq.com/license.txt
+ * @link     ladkalsdf  sdf 
+ */
+class bcName extends AnotherClass implements Interface
+{
 
-echo "This is 小胖\'s file.";
-// https://tiffanybbrown.com/2017/06/using-multiple-ssh-keys-with-github/index.html
-// https://stackoverflow.com/questions/29023532/how-do-i-use-multiple-ssh-keys-on-github
-// Host user1-github
-//     HostName github.com
-//     User git
-//     IdentityFile ~/.ssh/user1_rsa
-// Host user2-github
-//     HostName github.com
-//     User git
-//     IdentityFile ~/.ssh/user2_rsa
+    echo "This is 小胖\'s file.";
+    /**
+     * https://tiffanybbrown.com/2017/06/using-multiple-ssh-keys-with-github/index.html
+    
+        HostName github.com
+        User git
+        IdentityFile ~/.ssh/user1_rsa
+    
+        HostName github.com
+        User git
+        IdentityFile ~/.ssh/user2_rsa 
+     */
 
-// test Using multiple SSH keys with GitHub
+    // test Using multiple SSH keys with GitHub
 
+    // add branch dev-local 2018-09-26 13:25:23
+    // add somethings , commint 2 . 2018-09-26 13:26:40
+    echo "Wed Sep 26 14:46:14 CST 2018";
 
-// add branch dev-local 2018-09-26 13:25:23
-// add somethings , commint 2 . 2018-09-26 13:26:40
+    $abcVar=0
+    if ($abcVar > 1) {return true;
+        echo 'a';
+    }
+    if (false) {return false;
+        echo 'b';
+    }
+}
