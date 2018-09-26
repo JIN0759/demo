180b4587b3cafdb54df72d25c96c3e83a9a8a285
u1005 <u1005@ijuzhong.com>
2018-09-26 12:41:00 +0800

    mod index

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo 'This is 小胖.';
+/**
+ * Doc comment
+ * 
+ * PHP version 7.1
+ * 
+ * @category AaldkFakdkd
+ * @package  AdkFkd
+ * @author   Xia Gw <xiagw@qq.com>
+ * @license  ladk http://license.qq.com/license.txt
+ * @link     ladkalsdf  sdf 
+ */
+// 2018-09-26 12:35:33
+
+namespace pangzi
+
+// use admin\pangzi
+
+echo "This is 小胖\'s file.";
