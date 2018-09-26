6633dbada24cc45e7f30f2dfb9cdd0f4d46d8f31
u1005 <u1005@ijuzhong.com>
2018-09-26 13:35:55 +0800

    add url for multiple-user

--- index.php
+++ index.php
@@ -18,7 +18,19 @@ namespace pangzi
 // use admin\pangzi
 
 echo "This is 小胖\'s file.";
+// https://tiffanybbrown.com/2017/06/using-multiple-ssh-keys-with-github/index.html
+// https://stackoverflow.com/questions/29023532/how-do-i-use-multiple-ssh-keys-on-github
+// Host user1-github
+//     HostName github.com
+//     User git
+//     IdentityFile ~/.ssh/user1_rsa
+// Host user2-github
+//     HostName github.com
+//     User git
+//     IdentityFile ~/.ssh/user2_rsa
+
 // test Using multiple SSH keys with GitHub
+
 
 // add branch dev-local 2018-09-26 13:25:23
 // add somethings , commint 2 . 2018-09-26 13:26:40
